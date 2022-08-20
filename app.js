a279eed051db64e29802bc6ba53bf0e9981c157b
KHegedus1 <kico.kiki@gmail.com>
2022-08-20 23:05:42 +0200

    listen

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const server = http.createServer(function(req,res){
 
 })
 
+server.listen(port,function(error){
+    if(error){
+        console.log('Something went wrong',error)
+    }else{
+        console.log('Server is listening on port' + port)
+    }
+})
